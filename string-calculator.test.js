7acccd90006dc5ec990062b768aaca0728be356b
adibfiroz <adibfiroz9702@gmail.com>
2024-06-28 14:48:20 +0530

    Case2 - One Number

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,6 +1,12 @@
 const add = require("./string-calculator");
 
-
+//Case1 - Empty String
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
+});
+
+//Case2 - One Number
+test('returns the number itself when a single number is provided', () => {
+    expect(add("1")).toBe(1);
+    expect(add("42")).toBe(42);
 });
